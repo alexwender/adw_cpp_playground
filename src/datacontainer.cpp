011e425576c4082be5f4b75b1961fe43f2ec17dd
Alexander Wender <bosch.wender@extaccount.com>
2018-05-01 17:57:40 +0200

    bugfix
    Branch: master

--- src/datacontainer.cpp
+++ src/datacontainer.cpp
@@ -10,42 +10,7 @@ namespace playground {
 
 
 
-template class DataContainer<DataObject<float> >;
 
-template<typename T>
-bool DataContainer<T>::addDataByValue(const DataContainer::value_type data)
-{
 
-    this->m_data_container.emplace_back(data);
-
-    return true;
-
-}
-
-template<typename T>
-bool DataContainer<T>::addDataByReference(const DataContainer::value_type &data)
-{
-    this->m_data_container.emplace_back(data);
-
-}
-
-template<typename T>
-bool DataContainer<T>::addDataByRRef(DataContainer::value_type &&data)
-{
-    this->m_data_container.emplace_back(std::move(data));
-
-}
-
-template<typename T>
-void DataContainer<T>::print()
-{
-
-    size_t counter = 0;
-    for (const auto& elem: this->m_data_container) {
-        std::cout << "["  << std::setw(12) << counter << "]" << elem.getString() << "\n";
-        counter++;
-    }
-
-}
 
 } /* namespace playground */
